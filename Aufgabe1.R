c48852b7ea1d54bc087e4c482e59d72017018e82
pl145 <pauldickmann2006@gmail.com>
2026-01-12 20:53:08 +0100

    Aufgabe 1 b
    Lösen der Teilaufgabe b) und formulieren der Aufgabenstellung von c)

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -4,10 +4,14 @@ datensatz <- read.csv("E:/Uni/Semester 3/Wissarbeiten/titanic.csv")
 
 # a)
 
-#ohne packages(vielleicht leichter mit)
-titel <- sub('.*, ([A-Za-z]+)\\..*', '\\1', datensatz$Name)
+# Anrede herausfiltern
+Anrede <- sub('.*, ([A-Za-z]+)\\..*', '\\1', datensatz$Name)
 
 # b)
-
-
+# Codieren der Variablen "Survived","Sex" und "Embarked" als factor
+datensatz$Survived <- factor(datensatz$Survived, levels = c(0, 1), labels = c("No", "Yes"))
+datensatz$Sex <- factor(datensatz$Sex, levels = c("", "female"))
+datensatz$Embarked <- factor(datensatz$Embarked, levels = c("C", "Q", "S"),
+labels = c("Cherbourg", "Queenstown", "Southampton"))
 # c)
+# überführen der Variable "Pclass" in einen odered-factor
