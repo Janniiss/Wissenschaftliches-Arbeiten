bf59a8cda09fdb2b8cf21af4da3182c1ec75d7dc
pl145 <pauldickmann2006@gmail.com>
2026-01-13 19:40:34 +0100

    Aufgabe 1 c)
    Korrektur von Aufgabe b) und lösen von c)

--- Aufgabe1.R
+++ Aufgabe1.R
@@ -8,10 +8,13 @@ datensatz <- read.csv("E:/Uni/Semester 3/Wissarbeiten/titanic.csv")
 Anrede <- sub('.*, ([A-Za-z]+)\\..*', '\\1', datensatz$Name)
 
 # b)
+
 # Codieren der Variablen "Survived","Sex" und "Embarked" als factor
 datensatz$Survived <- factor(datensatz$Survived, levels = c(0, 1), labels = c("No", "Yes"))
-datensatz$Sex <- factor(datensatz$Sex, levels = c("", "female"))
+datensatz$Sex <- factor(datensatz$Sex, levels = c("male", "female"))
 datensatz$Embarked <- factor(datensatz$Embarked, levels = c("C", "Q", "S"),
 labels = c("Cherbourg", "Queenstown", "Southampton"))
+
 # c)
-# überführen der Variable "Pclass" in einen odered-factor
+# überführen der Variable "Pclass" in einen ordered-factor
+datensatz$Pclass <- factor(datensatz$Pclass, levels = c(1, 2, 3),ordered = TRUE)
