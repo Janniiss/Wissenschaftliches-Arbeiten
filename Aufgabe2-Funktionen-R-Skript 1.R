95f78d4e386f6268f7b7cfb08e3c4627184ef553
jhnns1810 <johannes.roehrig@tu-dortmund.de>
2026-01-25 12:01:13 +0100

    Update Aufgabe2-Funktionen-R-Skript 1.R

--- Aufgabe2-Funktionen-R-Skript 1.R
+++ Aufgabe2-Funktionen-R-Skript 1.R
@@ -83,6 +83,12 @@ library(vcd)  # Für Assoziationsmaße wie Phi-Koeffizient
 # Funktion: Deskriptive bivariate Statistik für den Zusammenhang zwischen zwei kategoriale Variablen
 deskriptive_bivariate_kategorial <- function(data, var1, var2) {
   
+  # Prüfen, ob eine der Variablen numerisch ist
+  # Falls ja: Abbruch, da die Funktion nur für kategoriale Variablen gedacht ist
+  if (is.numeric(data[[var1]]) || is.numeric(data[[var2]])) {
+    stop("Beide Variablen müssen kategorial sein.")
+  }
+  
   # Kontingenztabelle
   cat("\nKontingenztabelle:\n")
   tbl <- table(data[[var1]], data[[var2]])
